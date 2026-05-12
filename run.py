2ea27b55e1b96a65436fba0ba75daf4da2ba30e3
vinney560 <vinneyjoy1@gmail.com>
2026-05-12 13:23:19 +0300

    Trued

--- run.py
+++
@@ -1,5 +0,0 @@
-if __name__ == "__main__":
-    import uvicorn
-    print("\n🔥 YT-DLP Streaming Server Running (4K/8K Ready)")
-    print("➡️  Open http://127.0.0.1:5000\n")
-    uvicorn.run("app.main:app", host="0.0.0.0", port=5000)
